4f1cba6f7a892bfa0b15cc4c88f5e0eb88baa5ac
Bird55 <bird55@narod.ru>
2015-04-16 19:34:50 +0300

    First commit

---
+++ app/src/main/java/ru/netis/bird/yartemp/ConfigActivity.java
@@ -0,0 +1,250 @@
+package ru.netis.bird.yartemp;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
+
+/**
+ * Created by bird on 19.03.2015
+ */
+public class ConfigActivity extends Activity {
+
+    int widgetID = AppWidgetManager.INVALID_APPWIDGET_ID;
+    Intent resultValue;
+    SharedPreferences sp;
+    Context context;
+    AppWidgetManager am;
+    boolean resultUnit = false;
+    boolean resultPeriod = false;
+
+    final String LOG_TAG = "myLogs";
+    final static boolean DEBUG = true;
+
+
+    public final static String YARTEMP_PREF = "yartemp_pref";
+    public final static String YARTEMP_DEGREES = "yartemp_degrees";
+    public final static String YARTEMP_PERIOD = "yartemp_period";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (DEBUG) Log.d(LOG_TAG, "ConfigActivity onCreate [?]");
+
+        // извлекаем ID конфигурируемого виджета
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        if (extras != null) {
+            widgetID = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID,
+                    AppWidgetManager.INVALID_APPWIDGET_ID);
+        }
+        // и проверяем его корректность
+        if (widgetID == AppWidgetManager.INVALID_APPWIDGET_ID) {
+            finish();
+        }
+
+        if (DEBUG) Log.d(LOG_TAG, "ConfigActivity onCreate [" + widgetID + "]");
+
+        // формируем intent ответа
+        resultValue = new Intent();
+        resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetID);
+
+        // отрицательный ответ
+        setResult(RESULT_CANCELED, resultValue);
+        // положительный ответ
+        setResult(RESULT_OK, resultValue);
+
+        sp = getSharedPreferences(YARTEMP_PREF, MODE_PRIVATE);
+
+        context = getBaseContext();
+        am = AppWidgetManager.getInstance(context);
+
+        setContentView(R.layout.config_layout);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (DEBUG) Log.d(LOG_TAG, "ConfigActivity onDestroy [" + widgetID + "]");
+
+/*
+        String str = sp.getString(ConfigActivity.YARTEMP_PERIOD + widgetID, null);
+        if (DEBUG) Log.d(LOG_TAG, "1");
+        if (!resultPeriod && str == null) {
+            if (DEBUG) Log.d(LOG_TAG, "2");
+            setSharedPreferences(YarTemp.DEFAULT_PERIOD);
+        }
+        if (DEBUG) Log.d(LOG_TAG, "3");
+        int i = sp.getInt(ConfigActivity.YARTEMP_PERIOD + widgetID, -1);
+        if (DEBUG) Log.d(LOG_TAG, "4");
+        if (!resultUnit  && i == -1) {
+            if (DEBUG) Log.d(LOG_TAG, "5");
+            setSharedPreferences(YarTemp.DEFAULT_UNIT);
+        }
+        if (DEBUG) Log.d(LOG_TAG, "6");
+*/
+
+        YarTemp.updateWidget(context, am, sp, widgetID);
+    }
+
+    @Override
+    protected void onStop() {
+        if (DEBUG) Log.d(LOG_TAG, "ConfigActivity onStop [" + widgetID + "]");
+        super.onStop();
+    }
+
+    public void onClickUnit(View v) {
+        if (DEBUG) Log.d(LOG_TAG, "ConfigActivity onClickUnit [" + widgetID + "]");
+
+        Dialog dialog = onCreateUnitDialog();
+        dialog.show();
+//        onPause();
+    }
+
+    public void onClickRefresh(View v) {
+        if (DEBUG) Log.d(LOG_TAG, "ConfigActivity onClickRefresh [" + widgetID + "]");
+
+        Dialog dialog = onCreatePeriodDialog();
+        dialog.show();
+//        onPause();
+    }
+
+    public void setSharedPreferences(int unit) {
+        if ((DEBUG)) Log.d(LOG_TAG, "ConfigActivity setSharedPreferences [" + widgetID + "] " +
+                "Записываем в Preferences единицы измерения = "
+                + (unit == YarTemp.CELSIUS ? context.getString(R.string.degree_celsius) : context.getString(R.string.degree_fahrenheit)));
+        // Записываем значения с экрана в Preferences
+        Editor editor = sp.edit();
+        editor.putInt(YARTEMP_DEGREES + widgetID, unit);
+        editor.apply();
+    }
+
+    public void setSharedPreferences(String refresh) {
+        if ((DEBUG)) Log.d(LOG_TAG, "ConfigActivity setSharedPreferences [" + widgetID + "] " +
+                "Записываем в Preferences интерва обновления = " + refresh);
+        // Записываем значения с экрана в Preferences
+        Editor editor = sp.edit();
+        editor.putString(YARTEMP_PERIOD + widgetID, refresh);//.toString();
+        editor.apply();
+    }
+
+
+    public Dialog onCreateUnitDialog() {
+
+        int degreeUnit;
+
+        //Initialize the Alert Dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        //Source of the data in the Dialog
+        final CharSequence[] array = {context.getString(R.string.degree_celsius), context.getString(R.string.degree_fahrenheit)};
+
+        final SharedPreferences sp = getSharedPreferences(ConfigActivity.YARTEMP_PREF, MODE_PRIVATE);
+
+        degreeUnit = sp.getInt(ConfigActivity.YARTEMP_DEGREES + widgetID, YarTemp.CELSIUS);
+        // Set the dialog title
+        builder.setTitle(context.getString(R.string.degree_unit_conf))
+                .setCancelable(false)
+                // Specify the list array, the items to be selected by default (null for none),
+                // and the listener through which to receive callbacks when items are selected
+                .setSingleChoiceItems(array, degreeUnit, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (DEBUG) Toast.makeText(
+                                getApplicationContext(),
+                                context.getString(R.string.degree_unit_conf)
+                                        + " " + array[which],
+                                Toast.LENGTH_SHORT).show();
+                    }
+                })
+
+                // Set the action buttons
+                .setPositiveButton(context.getString(R.string.btn_ok), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User clicked OK, so save the result somewhere
+                        // or return them to the component that opened the dialog
+                        ListView lv = ((AlertDialog) dialog).getListView();
+                        setSharedPreferences(lv.getCheckedItemPosition());
+                        resultUnit = true;
+                        dialog.cancel();
+                    }
+                })
+                .setNegativeButton(context.getString(R.string.btn_cancel), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        return builder.create();
+    }
+
+    public Dialog onCreatePeriodDialog() {
+
+        //Initialize the Alert Dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        //Source of the data in the Dialog
+        final String[] array = getResources().getStringArray(R.array.refresh_time_text);
+        final String[] value = getResources().getStringArray(R.array.refresh_time_value);
+
+        final SharedPreferences sp = getSharedPreferences(ConfigActivity.YARTEMP_PREF, MODE_PRIVATE);
+
+        String refreshValue = sp.getString(ConfigActivity.YARTEMP_PERIOD + widgetID, array[2]);
+        int refreshID = 2;
+        for (int i = 0; i <= array.length; i++) {
+            if (array[i].equals(refreshValue)) {
+                refreshID = i;
+                break;
+            }
+        }
+        // Set the dialog title
+        builder.setTitle(context.getString(R.string.time_update_conf))
+                .setCancelable(false)
+                        // Specify the list array, the items to be selected by default (null for none),
+                        // and the listener through which to receive callbacks when items are selected
+                .setSingleChoiceItems(array, refreshID, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (DEBUG) Toast.makeText(
+                                getApplicationContext(),
+                                context.getString(R.string.time_update_conf)
+                                        + " " + array[which],
+                                Toast.LENGTH_SHORT).show();
+                    }
+                })
+
+                        // Set the action buttons
+                .setPositiveButton(context.getString(R.string.btn_ok), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User clicked OK, so save the result somewhere
+                        // or return them to the component that opened the dialog
+                        ListView lv = ((AlertDialog) dialog).getListView();
+                        setSharedPreferences(value[lv.getCheckedItemPosition()]);
+                        resultPeriod = true;
+                        // положительный ответ
+                        setResult(RESULT_OK, resultValue);
+                        dialog.cancel();
+                    }
+                })
+                .setNegativeButton(context.getString(R.string.btn_cancel), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+        return builder.create();
+    }
+}
---
+++ app/src/main/java/ru/netis/bird/yartemp/YarTemp.java
@@ -0,0 +1,280 @@
+package ru.netis.bird.yartemp;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.RemoteViews;
+import android.widget.Toast;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.util.Arrays;
+import java.util.Calendar;
+
+/**
+ * Created by bird on 19.03.2015
+ */
+public class YarTemp extends AppWidgetProvider {
+
+    final static String ACTION_UPDATE = "ru.netis.bird.yartrmp.update";
+    final static String ACTION_GOSITE = "ru.netis.bird.yartrmp.goSite";
+
+    public static final int CELSIUS = 0;
+    public static final int FAHRENHEIT = 1;
+    public static final int DEFAULT_PERIOD = 120000;
+    public static final int DEFAULT_UNIT = CELSIUS;
+
+    final static String LOG_TAG = "myLogs";
+    final static boolean DEBUG = true;
+
+    static int degreeUnit;
+    static int periodUpdate;
+
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+        if (DEBUG) Log.d(LOG_TAG, "YarTemp onEnabled");
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager,
+                         int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+        if (DEBUG) Log.d(LOG_TAG, "YarTemp onUpdate " + Arrays.toString(appWidgetIds));
+
+        SharedPreferences sp = context.getSharedPreferences(
+                ConfigActivity.YARTEMP_PREF, Context.MODE_PRIVATE);
+        for (int id : appWidgetIds) {
+            updateWidget(context, appWidgetManager, sp, id);
+        }
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+
+        Log.d(LOG_TAG, "YarTemp onReceive");
+
+        // Извлекаем ID экземпляра
+        int mAppWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
+        Bundle extras = intent.getExtras();
+        Log.d(LOG_TAG, "YarTemp onReceive");
+        if (extras != null) {
+            mAppWidgetId = extras.getInt(
+                    AppWidgetManager.EXTRA_APPWIDGET_ID,
+                    AppWidgetManager.INVALID_APPWIDGET_ID);
+        }
+
+        Log.d(LOG_TAG, "YarTemp onReceive [" + mAppWidgetId + "]");
+        if (mAppWidgetId != AppWidgetManager.INVALID_APPWIDGET_ID) {
+            // Читаем значения
+            SharedPreferences sp = context.getSharedPreferences(
+                    ConfigActivity.YARTEMP_PREF, Context.MODE_PRIVATE);
+
+            // Проверяем, что это intent от нажатия на третью зону
+            if (intent.getAction().equalsIgnoreCase(ACTION_UPDATE)) {
+                Log.d(LOG_TAG, "YarTemp onReceive [" + mAppWidgetId + "] ACTION_UPDATE");
+                updateWidget(context, AppWidgetManager.getInstance(context), sp, mAppWidgetId);
+            }
+        }
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        super.onDeleted(context, appWidgetIds);
+        if (DEBUG) Log.d(LOG_TAG, "YarTemp onDeleted " + Arrays.toString(appWidgetIds));
+
+        // Удаляем Preferences
+        Editor editor = context.getSharedPreferences(
+                ConfigActivity.YARTEMP_PREF, Context.MODE_PRIVATE).edit();
+        for (int widgetID : appWidgetIds) {
+            editor.remove(ConfigActivity.YARTEMP_DEGREES + widgetID);
+            editor.remove(ConfigActivity.YARTEMP_PERIOD + widgetID);
+        }
+        editor.apply();
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        super.onDisabled(context);
+        if (DEBUG) Log.d(LOG_TAG, "YarTemp onDisabled");
+    }
+
+    static void updateWidget(Context context, AppWidgetManager appWidgetManager,
+                              SharedPreferences sp, int widgetID) {
+        PendingIntent pIntent;
+        Log.d(LOG_TAG, "YarTemp updateWidget [" + widgetID + "]");
+
+        RemoteViews widgetView = new RemoteViews(context.getPackageName(), R.layout.widget_lqyout);
+
+// текст "uploading..."
+        widgetView.setTextViewText(R.id.Updated, context.getString(R.string.loading));
+
+        // Site yartemp.com (певая зона)
+        Uri uri = Uri.parse("http://yartemp.com");
+        Intent siteIntent = new Intent(Intent.ACTION_VIEW, uri);
+        pIntent = PendingIntent.getActivity(context, widgetID, siteIntent, 0);
+        widgetView.setOnClickPendingIntent(R.id.llImage, pIntent);
+
+        // Конфигурационный экран (вторая зона)
+        Intent configIntent = new Intent(context, ConfigActivity.class);
+        configIntent.setAction(AppWidgetManager.ACTION_APPWIDGET_CONFIGURE);
+        configIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetID);
+        pIntent = PendingIntent.getActivity(context, widgetID, configIntent, 0);
+        widgetView.setOnClickPendingIntent(R.id.llText, pIntent);
+
+        // Экран обновления (третяя зона)
+        Intent updateIntent = new Intent(context, YarTemp.class);
+        updateIntent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+        updateIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, new int[] {widgetID});
+        pIntent = PendingIntent.getBroadcast(context, widgetID, updateIntent, 0);
+        widgetView.setOnClickPendingIntent(R.id.llUpdate, pIntent);
+
+        appWidgetManager.updateAppWidget(widgetID, widgetView);
+
+        // Читаем параметры Preferences
+        String periodSt = sp.getString(ConfigActivity.YARTEMP_PERIOD + widgetID, null);
+        degreeUnit = sp.getInt(ConfigActivity.YARTEMP_DEGREES + widgetID, -1);
+
+        periodUpdate = Integer.getInteger(periodSt, DEFAULT_PERIOD);
+        degreeUnit = sp.getInt(ConfigActivity.YARTEMP_DEGREES + widgetID, DEFAULT_UNIT);
+
+        if (DEBUG) Log.d(LOG_TAG, "YarTemp updateWidget [" + widgetID + "] degree unit = " + degreeUnit + " period update = " + periodUpdate);
+
+        // Проверяем подключение к сети
+        ConnectivityManager connMgr = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+            ParseURL parseURL = new ParseURL();
+            parseURL.setContext(context);
+            parseURL.setAm(appWidgetManager);
+            parseURL.setWidgetID(widgetID);
+            parseURL.execute(context.getString(R.string.temp_URL));
+        } else {
+
+            Toast.makeText(context, context.getString(R.string.network_error), Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    static class ParseURL extends AsyncTask<String, Void, String[]> {
+        Context context;
+        AppWidgetManager am;
+        int widgetID;
+
+        Element spTemp1;
+        Element spTemp1a;
+        String currentTempStr;
+        float currentTempFl;
+
+        Element spTemp2;
+        Element spTemp2a;
+        String deltaTempStr;
+        float deltaTempFl;
+
+        String currTime;
+
+        void setContext(Context context) {this.context = context;}
+        void setAm(AppWidgetManager am) {this.am = am;}
+        void setWidgetID(int widgetID) {this.widgetID = widgetID;}
+
+        @Override
+        protected String[] doInBackground(String... strings) {
+            String[] a = new String[]{"","",""};
+
+            try {
+                if (DEBUG) Log.d(LOG_TAG, "YarTemp doInBackground Connecting to [" + strings[0] + "]");
+                Document doc = Jsoup.connect(strings[0]).get();
+                Log.d(LOG_TAG, "YarTemp doInBackground Connected to [" + strings[0] + "]");
+
+                try {
+                    spTemp1 = doc.getElementById("spTemp1");
+                    spTemp1a = doc.getElementById("spTemp1a");
+                    currentTempStr = spTemp1.text() + spTemp1a.text();
+                    currentTempFl = Float.valueOf(currentTempStr);
+                    if (DEBUG) Log.d(LOG_TAG, "YarTemp doInBackground currentTemp = " + currentTempFl);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    if (DEBUG) Log.d(LOG_TAG, "YarTemp doInBackground [" + widgetID + "] currentTemp - ERROR");
+                }
+
+                try {
+                    spTemp2 = doc.getElementById("spTemp2");
+                    spTemp2a = doc.getElementById("spTemp2a");
+                    deltaTempStr = spTemp2.text() + spTemp2a.text();
+                    deltaTempFl = Float.valueOf(deltaTempStr);
+                    if (DEBUG) Log.d(LOG_TAG, "YarTemp doInBackground [" + widgetID + "] deltaTemp = " + deltaTempFl);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    if (DEBUG) Log.d(LOG_TAG, "YarTemp doInBackground [" + widgetID + "] deltaTemp - ERROR");
+                }
+
+                if (DEBUG) Log.d(LOG_TAG, "YarTemp doInBackground [" + widgetID + "] degreeUnit = " + degreeUnit);
+                deltaTempStr = context.getString(R.string.degree_speed); // "в час"
+                if (degreeUnit == FAHRENHEIT) {
+                    // Переводим °C в °F
+                    currentTempStr = context.getString(R.string.degrees_fahrenheit); // " °F"
+                    float sing = deltaTempFl > 0f ? 1f : -1f;
+                    currentTempFl = currentTempFl*5.0f/9.0f + 32.0f;
+                    deltaTempFl = sing * (deltaTempFl*5.0f/9.0f + 32.0f);
+                    a[0] = String.format("%3.0f %s", RoundSing(currentTempFl, 3), currentTempStr);
+                    a[1] = String.format("%+2.0f %s %s", RoundSing(deltaTempFl,2), currentTempStr, deltaTempStr);
+                } else {
+                    currentTempStr = context.getString(R.string.degrees_celsius); // " °C"
+                    a[0] = String.format("%2.0f %s", RoundSing(currentTempFl, 3), currentTempStr);
+                    a[1] = String.format("%+1.0f %s %s", RoundSing(deltaTempFl, 2), currentTempStr, deltaTempStr);
+                    if (DEBUG) Log.d(LOG_TAG, Float.toString(RoundSing(deltaTempFl, 2)));
+                }
+                currTime = getCurrentTime();
+                a[2] = context.getString(R.string.time_text) + " " + currTime; // "Обновлено в " + currTime
+
+                if (DEBUG) Log.d(LOG_TAG, "YarTemp doInBackground [" +widgetID + "]  " + a[0] + " | " + a[1] + " | " +a[2]);
+
+            } catch (Throwable t) {
+                t.printStackTrace();
+                if (DEBUG) Log.d(LOG_TAG, "YarTemp doInBackground [" +widgetID + "] network ERROR");
+            }
+
+            return a;
+        }
+
+        private String getCurrentTime() {
+            Calendar calendar = Calendar.getInstance();
+            int hour = calendar.get(Calendar.HOUR_OF_DAY);
+            int minute = calendar.get(Calendar.MINUTE);
+            return String.format("%02d:%02d", hour, minute); // ЧЧ:ММ - формат времени
+        }
+
+        @Override
+        protected void onPostExecute(String s[]) {
+            super.onPostExecute(s);
+
+            RemoteViews widgetView = new RemoteViews(context.getPackageName(), R.layout.widget_lqyout);
+            widgetView.setTextViewText(R.id.Temp,s[0]);
+            widgetView.setTextViewText(R.id.deltaTemp,s[1]);
+            widgetView.setTextViewText(R.id.Updated, s[2]);
+
+            am.updateAppWidget(widgetID, widgetView);
+
+        }
+    }
+
+    static float RoundSing(float x, int n){
+        if (x == 0f) return 0f;
+        int digits = n - (int)Math.log10((double)x) - 1;
+        if (x < 1) digits++;
+        return (float)(Math.round(x * Math.pow(10, digits)) / Math.pow(10, digits));    }
+}
